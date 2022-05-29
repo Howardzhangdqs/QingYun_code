add418dc35e06cfba8dd5f4674a586021d62f80d
Howard Zhang <75195784+Howardzhangdqs@users.noreply.github.com>
2022-05-29 16:21:35 +0800

    Add files via upload

--- activity/build.js
+++ activity/build.js
@@ -9,31 +9,32 @@
 			t.setAttribute("class", "container")
 			for (let i = 0; i < data.length; i ++) {
 				let m;
-				if (data[i].m != undefined && data[i].m != "") {
-					m = data[i].m;//"<iframe style='width:100%;height:338px' frameborder='no' allowfullscreen mozallowfullscreen webkitallowfullscreen src='" + data[i].m + "'></iframe>"
-				} else {
-					m = "<img src='" + data[i].pic + "' alt='' loaded='images/intro/pic_1.minn.png'/>"
+				if (data[i].pic == "") t.innerHTML += '<div class="row gtr-150" id="act' + i +
+					'"><div class="col-12-medium" style="text-align:center"><header class="major" style="text-align:center"><h3>' + data[i].title +
+					'</h3></header><p class="para">' + data[i].content + '</p><a href="' + data[i].link + '" target="_blank" class="button">查看原文</a></div></div>';
+				else {
+					if (data[i].m != undefined && data[i].m != "") {
+						m = data[i].m;
+					} else {
+						m = "<img src='" + data[i].pic + "' alt='' loaded='images/intro/pic_1.minn.png'/>"
+					}
+					if (i % 2 == 0) {
+						t.innerHTML += '<div class="row gtr-150" id="act' + i +
+						'"><div class="col-6 col-12-medium" style=""><ul class="major-icons"><li><span class="image fit">' + m +
+						'</span></li></ul></div><div class="col-6 col-12-medium"><header class="major"><h3>' + data[i].title +
+						'</h3></header><p class="para">' + data[i].content + '</p><a href="' + data[i].link + '" target="_blank" class="button" style="float:left">查看原文</a></div></div>';
+					} else {
+						t.innerHTML += '<div class="row gtr-150" id="act' + i +
+						'"><div class="col-6 col-12-medium spe-col" style=""><header class="major"><h3>' + data[i].title +
+						'</h3></header><p class="para">' + data[i].content +
+						'</p><a href="' + data[i].link + '" target="_blank" class="button" style="float:right">查看原文</a></div><div class="col-6 col-12-medium"><ul class="major-icons"><li><span class="image fit">' + m +
+						'</span></li></ul></div></div>';
+					}
 				}
-				if (i % 2 == 0) {
-					t.innerHTML += `<div class="row gtr-150">
-							<div class="col-6 col-12-medium"><ul class="major-icons"><li><span class="image fit">
-								` + m + `
-							</span></li></ul></div>
-							<div class="col-6 col-12-medium">
-								<header class="major"><h3>` + data[i].title + `</h3></header>
-								<p class="para">` + data[i].content + `</p>
-							</div>
-						</div>`;
-				} else {
-					t.innerHTML += `<div class="row gtr-150"><div class="col-6 col-12-medium spe-col">
-								<header class="major"><h3>` + data[i].title + `</h3></header><p class="para">` + data[i].content + `</p>
-							</div><div class="col-6 col-12-medium"><ul class="major-icons"><li><span class="image fit">
-									` + m + `
-								</span></li></ul></div></div>
-					`;
-				}
+
 			}
 			document.getElementById("two").appendChild(t);
+			$(function() {$.scrollTo('#' + ((window.location.href).split("#")[1]),1000)})
 		}).catch(function(error) {setTimeout(build, dtime)});
 	}; build();
 })();
